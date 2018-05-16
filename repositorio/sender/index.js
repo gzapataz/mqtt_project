586ae0160864f5b5467987a0ab9dae953ab5c63a
Gabriel Zapata <gab.zapata@uniandes.edu.co>
2018-05-16 15:32:09 -0500

    Cambios en Sensor

--- repositorio/sender/index.js
+++ repositorio/sender/index.js
@@ -1,10 +1,20 @@
 var mqtt    = require('mqtt');
+var sensorLib = require('node-dht-sensor');
 var client  = mqtt.connect(process.env.BASE_MQTT_URL);
+var client_id = process.env.BASE_IOT_CLIENTID
 
 console.log('start sender message');
 var temp = 0;
 var message ='';
 var i=0;
+
+// Setup sensor, exit if failed
+var sensorType = 22; // DHT22, AM2302
+var sensorPin  = 4;  // The GPIO pin number for sensor signal
+if (!sensorLib.initialize(sensorType, sensorPin)) {
+    console.warn('No se inicializo el sensor');
+    process.exit(1);
+}
 
 
 // Connection to topics: Temperatura, Voltaje y Humedad in mosquitto
@@ -13,17 +23,21 @@ client.on('connect', function () {
 		temp = newtempReading();
 		var jsonTemp = {
 			value : temp,
-			client_id: 31231231231
+			client_id: client_id
 		};
 		message = JSON.stringify(jsonTemp);
 		console.log(message);
 		client.publish('temperatura', message);
-	}, 1000)
+	}, 3000) //meke a sensor reading every second
 	
 		//client.publish('voltaje', true);
 		//client.publish('humedad', true);
 });
 
+
+// Read temperature sensor 
 function newtempReading() {
-	return Math.random()*10;
+	var readout = sensorLib.read();
+    return readout;
+	//return Math.random()*10; 
 }

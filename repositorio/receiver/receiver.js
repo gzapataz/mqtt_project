943454cd88363dcbe2164b65eba7d2145c2ccca6
Gabriel Zapata <gab.zapata@uniandes.edu.co>
2018-05-18 21:44:00 -0500

    Fix dependencia

--- repositorio/receiver/receiver.js
+++ repositorio/receiver/receiver.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
-var router = require('./router');
 var pg = require('pg');
 var AWS = require("aws-sdk");
 
@@ -14,24 +13,23 @@ var client_id = process.env.BASE_IOT_CLIENTID
 //app.use(bodyParser.json())
 
 client.on('connect', function () {
-
-app.get('/light', function(req, res) {
-	temp = req.query.val;
-	console.log('Light:' + temp)
-	
+	console.log ('Conectado Colas...')
+	app.get('/light', function(req, res) {
+		temp = req.query.val;
+		console.log('Light:' + temp)
 		
-		var jsonLight = {
-			value : temp,
-			clientid: client_id
-		};
-		message = JSON.stringify(jsonLight);
-		console.log(message);
-		client.publish('voltaje', message);
-
-
-});
+			
+			var jsonLight = {
+				value : temp,
+				clientid: client_id
+			};
+			message = JSON.stringify(jsonLight);
+			console.log(message);
+			client.publish('voltaje', message);
+	
+	
 	});
-//router(app);
+});
 
 console.log ('Salio')
 app.listen(8080, () => {
